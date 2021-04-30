eb4bdfc989e6901e6cf490ee1de10b0a98e88189
ShivamYadav <cse.18bcs2337@gmail.com>
2021-04-30 15:16:42 +0530

    generic welcome added

--- src/app/chat/messages/chat-head/chat-head.component.ts
+++ src/app/chat/messages/chat-head/chat-head.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Chat } from 'src/app/shared/chat.model';
 import { ChatService } from 'src/app/shared/chat.service';
+import { User } from 'src/app/shared/user.model';
 
 @Component({
 	selector: 'app-chat-head',
@@ -16,6 +17,7 @@ export class ChatHeadComponent implements OnInit, OnDestroy {
 	constructor(private chatService: ChatService) { }
 
 	ngOnInit(): void {
+		this.chat = new Chat('0', 'individual', [new User('0','vibez', '', '','','')], []);
 		this.chatSwitchedSubscription = this.chatService.chatSwitched.subscribe((id)=>{
 			this.chat = this.chatService.getChatByChatId(id);
 		});
